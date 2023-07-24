71be45ba090443d39eadf078950f6a6aed094db6
sadapannord <sadapan@gmail.com>
2023-07-23 20:08:28 -0600

    adding some navigation

--- thenordplace/src/components/Container.js
+++ thenordplace/src/components/Container.js
@@ -1,36 +1,37 @@
 import React, { useState } from "react";
-// import NavTabs from "./NavTabs";
-// import About from "./pages/About";
-// import Contact from "./pages/Contact";
+import NavTabs from "./NavTabs";
+import Paracord from "./pages/Paracord";
+import Sewed from "./pages/Sewed";
+import Stickers from "./pages/Stickers";
 import Footer from "./footer/index";
 import Header from "./header/index";
 
 export default function Container() {
-  //   const [currentPage, setCurrentPage] = useState("Home");
+  const [currentPage, setCurrentPage] = useState("Home");
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
-  //   const renderPage = () => {
-  //     if (currentPage === "Contact") {
-  //       return <Contact />;
-  //     }
-  //     if (currentPage === "Project") {
-  //       return <Projects />;
-  //     }
-  //     if (currentPage === "Resume") {
-  //       return <Resume />;
-  //     }
-  //     return <About />;
-  //   };
+  const renderPage = () => {
+    if (currentPage === "Contact") {
+      return <Paracord />;
+    }
+    if (currentPage === "Project") {
+      return <Sewed />;
+    }
+    if (currentPage === "Resume") {
+      return <Stickers />;
+    }
+    return <About />;
+  };
 
-  //   const handlePageChange = (page) => setCurrentPage(page);
+  const handlePageChange = (page) => setCurrentPage(page);
 
   return (
     <div>
       <Header />
       {/* We are passing the currentPage from state and the function to update it */}
-      {/* <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} /> */}
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
       {/* Here we are calling the renderPage method which will return a component  */}
-      {/* {renderPage()} */}
+      {renderPage()}
       <Footer />
     </div>
   );
--- thenordplace/src/components/NavTabs.js
+++ thenordplace/src/components/NavTabs.js
@@ -5,40 +5,35 @@ function NavTabs({ currentPage, handlePageChange }) {
     <ul className="nav nav-tabs">
       <li className="nav-item">
         <a
-          href="#about"
+          href="#paracord"
           onClick={() => handlePageChange("About")}
-          className={currentPage === "About" ? "nav-link active" : "nav-link"}
+          className={
+            currentPage === "Paracord" ? "nav-link active" : "nav-link"
+          }
         >
-          About
+          Paracord
         </a>
       </li>
       <li className="nav-item">
         <a
-          href="#contact"
-          onClick={() => handlePageChange("Contact")}
-          className={currentPage === "Contact" ? "nav-link active" : "nav-link"}
+          href="#Sewed"
+          onClick={() => handlePageChange("Sewed")}
+          className={currentPage === "Sewed" ? "nav-link active" : "nav-link"}
         >
-          Contact
+          Sewed
         </a>
       </li>
       <li className="nav-item">
         <a
-          href="#project"
-          onClick={() => handlePageChange("Project")}
-          className={currentPage === "Project" ? "nav-link active" : "nav-link"}
+          href="#stickers"
+          onClick={() => handlePageChange("Stickers")}
+          className={
+            currentPage === "Stickers" ? "nav-link active" : "nav-link"
+          }
         >
-          Project
+          Stickers
         </a>
       </li>
-      {/* <li className="nav-item">
-        <a
-          href="#resume"
-          onClick={() => handlePageChange("Resume")}
-          className={currentPage === "Resume" ? "nav-link active" : "nav-link"}
-        >
-          Resume
-        </a>
-      </li> */}
     </ul>
   );
 }
