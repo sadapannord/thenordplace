8d5ce723d76eb643b647c485ca19786f292ed9f2
sadapannord <sadapan@gmail.com>
2023-08-06 19:05:42 -0600

    adding more contact info

--- thenordplace/src/components/footer/index.js
+++ thenordplace/src/components/footer/index.js
@@ -33,6 +33,11 @@ const Footer = () => {
           </div>
         </div>
         <div>
+          <p>
+            {" "}
+            Please feel free to contact me if you have any questions or
+            requests!
+          </p>
           <p>Contact</p>
           <ul>Phone: 303-332-7351</ul>
           <ul>Email: sadapan@gmail.com</ul>
