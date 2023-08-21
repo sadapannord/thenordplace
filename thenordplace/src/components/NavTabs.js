10569f0ee8760965dd599e50a3ad3e26935a5074
sadapannord <sadapan@gmail.com>
2023-08-20 19:11:08 -0600

    adding styling

--- thenordplace/src/components/NavTabs.js
+++ thenordplace/src/components/NavTabs.js
@@ -2,39 +2,41 @@ import React from "react";
 
 function NavTabs({ currentPage, handlePageChange }) {
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <a
-          href="#paracord"
-          onClick={() => handlePageChange("Paracord")}
-          className={
-            currentPage === "Paracord" ? "nav-link active" : "nav-link"
-          }
-        >
-          Paracord
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#sewed"
-          onClick={() => handlePageChange("Sewed")}
-          className={currentPage === "Sewed" ? "nav-link active" : "nav-link"}
-        >
-          Sewed
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#stickers"
-          onClick={() => handlePageChange("Stickers")}
-          className={
-            currentPage === "Stickers" ? "nav-link active" : "nav-link"
-          }
-        >
-          Stickers
-        </a>
-      </li>
-    </ul>
+    <div class="row">
+      <ul className="nav nav-tabs">
+        <ul class="column">
+          <a
+            href="#paracord"
+            onClick={() => handlePageChange("Paracord")}
+            className={
+              currentPage === "Paracord" ? "nav-link active" : "nav-link"
+            }
+          >
+            Paracord
+          </a>
+        </ul>
+        <ul class="column">
+          <a
+            href="#sewed"
+            onClick={() => handlePageChange("Sewed")}
+            className={currentPage === "Sewed" ? "nav-link active" : "nav-link"}
+          >
+            Sewed
+          </a>
+        </ul>
+        <ul class="column">
+          <a
+            href="#stickers"
+            onClick={() => handlePageChange("Stickers")}
+            className={
+              currentPage === "Stickers" ? "nav-link active" : "nav-link"
+            }
+          >
+            Stickers
+          </a>
+        </ul>
+      </ul>
+    </div>
   );
 }
 
