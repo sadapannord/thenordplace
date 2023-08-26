2b2a745effc32176f77972078456cf13fcb7aabd
sadapannord <sadapan@gmail.com>
2023-08-26 12:32:00 -0600

    changing some styling

--- thenordplace/src/components/pages/Home.js
+++ thenordplace/src/components/pages/Home.js
@@ -4,7 +4,10 @@ import React from "react";
 export default function Home() {
   return (
     <div className="row">
-      <h3>Hello! My name is Sada, and I'm a full stack developer.</h3>
+      <h3>
+        Hello! My name is Sada, and I'm a full stack developer with a penchant
+        for crafting in my spare time.
+      </h3>
       <p></p>
       <p></p>
     </div>
